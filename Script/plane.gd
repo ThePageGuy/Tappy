217d57a6f243f936799b94f0eb2f0f4518cbf4bc
The Page Guy <thepageguy@mailfence.com>
2024-12-19 18:36:36 +0000

    Decompress Zip File

---
+++ Script/plane.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+
+@onready var anim_sprite = $AnimSprite
+@onready var anim_player = $AnimPlayer
+
+const GRAVITY = 300.0
+const POWER = -200.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+func _physics_process(delta):
+	
+	velocity.y += GRAVITY * delta
+	
+	fly()
+	
+	move_and_slide()
+	
+	if is_on_floor() == true:
+		die()
+
+
+func fly():
+	if Input.is_action_just_pressed("Fly") == true:
+		velocity.y = POWER
+		anim_player.play("power")
+
+
+func die():
+	anim_sprite.stop()
+	set_physics_process(false)
